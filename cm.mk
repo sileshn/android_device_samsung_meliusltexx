LineageOS/android_device_samsung_serranoltexx

Initial N bringup
Change-Id: If2f88b8b0062e63ddb1f743495125da7682cacb1

cm.mk
@@ -1,11 +1,35 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from serranoltexx device
+$(call inherit-product, device/samsung/serranoltexx/device.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-$(call inherit-product, device/samsung/serranoltexx/full_serranoltexx.mk)
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=serranoltexx TARGET_DEVICE=serranolte
+# Also get non-open-source specific aspects
+$(call inherit-product, vendor/samsung/serrano-common/serrano-common-vendor.mk)
 
 PRODUCT_NAME := cm_serranoltexx
+PRODUCT_DEVICE := serranoltexx
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := Samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=serranoltexx TARGET_DEVICE=serranolte

sileshn/android_device_samsung_meliusltexx

cm.mk
@@ -1,5 +1,4 @@
-#
-# Copyright (C) 2013 The CyanogenMod Project
+# Copyright (C) 2016 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +11,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from meliusltexx device
+$(call inherit-product, device/samsung/meliusltexx/device.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
@@ -20,20 +24,12 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-$(call inherit-product, device/samsung/meliusltexx/full_meliusltexx.mk)
+# Also get non-open-source specific aspects
+$(call inherit-product, vendor/samsung/melius-common/melius-common-vendor.mk)
 
-# product
-PRODUCT_DEVICE := meliusltexx
-PRODUCT_BRAND := Samsung
 PRODUCT_NAME := cm_meliusltexx
-PRODUCT_MODEL := GT-I9205
-PRODUCT_MANUFACTURER := samsung
+PRODUCT_DEVICE := meliusltexx
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := Samsung
 
-# OTA
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.rom.device=cm
-
-# override
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="meliusltexx-user 4.4.2 KOT49H I9205XXUDNE4 release-keys" \
-    BUILD_FINGERPRINT="samsung/meliusltexx/meliuslte:4.4.2/KOT49H/I9205XXUDNE4:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=meliusltexx TARGET_DEVICE=meliuslte